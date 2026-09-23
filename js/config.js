3fcf20430b5f84fb8fc93ccd9272c6e27742032d
FATUR RIZKY FAHRIANSYAH <faturrizkyfahriansyah12@gmail.com>
2026-09-24 03:05:22 +0700

    Update config.js

--- js/config.js
+++ js/config.js
@@ -3,6 +3,6 @@
  * SATU-SATUNYA file yang perlu diedit setelah deploy Apps Script (docs/DEPLOYMENT.md TAHAP 11).
  * Isi apiBaseUrl dengan URL Web App hasil "Deploy > New deployment" di Apps Script.
  */
-window.TOKO_AZKIA_CONFIG = {
+window.TOKO_AZQIA_CONFIG = {
   apiBaseUrl: 'https://script.google.com/macros/s/AKfycbxJM_-KILXmJhOaZHjD40-CXGv620apnKLyYnMXZD3mFI6QNIDkz_leZHQwr6JQlbRISQ/exec'
 };
